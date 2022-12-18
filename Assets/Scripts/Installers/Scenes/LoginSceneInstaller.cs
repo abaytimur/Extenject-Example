7c9cd9c4a9f0e04dc1b8aa0f35e8aba174270703
kbaytimur <a_baytimur@hotmail.com>
2022-12-19 02:20:56 +0300

    Update.

---
+++ Assets/Scripts/Installers/Scenes/LoginSceneInstaller.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+namespace Installers.Scenes
+{
+    public class LoginSceneInstaller : MonoInstaller<LoginSceneInstaller>
+    {
+        private const int MainSceneBuildIndex = 1;
+
+        public override void InstallBindings()
+        {
+        }
+
+        private void Awake()
+        {
+            SceneManager.LoadScene(MainSceneBuildIndex);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Installers/Scenes/MainSceneInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace Installers.Scenes
+{
+  public class MainSceneInstaller : MonoInstaller<MainSceneInstaller>
+  {
+    public override void InstallBindings()
+    {
+    }
+  }
+}
